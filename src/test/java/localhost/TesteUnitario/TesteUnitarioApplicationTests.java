bfa5ed0e8c621f8613be69889aead9f09f13ca40
Adalberto Nascimento <adalbertos.nascimento@gmail.com>
2021-05-01 15:16:35 -0300

    Entrega provisória. Falta implentar.

---
+++ src/test/java/localhost/TesteUnitario/TesteUnitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package localhost.TesteUnitario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteUnitarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
